aa4cf202d2b9a6d75b2a5c8a347169fdee67d9ed
Yeferson Flores <yefersonflores15@gmail.com>
2022-10-09 19:57:26 -0500

    feat: form under WTForm was created and tested

---
+++ rutas.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from forms import FormInicio
+import os
+
+SECRET_KEY = os.urandom(32)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+@app.route('/')
+def inicio():
+    return render_template('base.html')
+@app.route('/index')
+def index():
+    usuario = {'usuario':'...'}
+    comentarios = [
+        {'autor':{'usuario':'...'}, 'comentario':'...'},
+        {'autor':{'usuario':'...'}, 'comentario':'...'}
+    ]
+    return render_template('index.html', titulo='Inicio', usuario=usuario, comentarios=comentarios)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = FormInicio()
+    if(form.validate_on_submit()):
+        flash('Inicio de sesion solicitado por el usuario {}, recordar={}'.format(form.usuario.data,
+            form.recordar.data))
+        return redirect(url_for('gracias'))
+    return render_template('iniciar_sesion.html', titulo='Iniciar Sesión', form=form)
+
+@app.route('/gracias')
+def gracias():
+    return render_template('gracias.html')
